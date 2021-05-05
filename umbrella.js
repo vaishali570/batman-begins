5aec9253dfff5a26bcd063e20f6e0e22fb39003d
vaishali570 <76772213+vaishali570@users.noreply.github.com>
2021-05-05 01:56:40 -0700

    Add files via upload

---
+++ umbrella.js
@@ -0,0 +1,18 @@
+class Umbrella{
+    constructor(x,y){
+        var options={
+            isStatic:true
+        }
+        this.body=Bodies.circle(x,y,50,options)
+        this.radius=50;
+        this.image = loadImage("Walking Frame/walking_1.png");
+        
+        World.add(world,this.body)
+    }
+display(){
+    var pos=this.body.position;
+    imageMode(CENTER)
+    image(this.image,pos.x,pos.y+70,300,300)
+}
+
+}
